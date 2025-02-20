d0d20e3437948579340913dd9518ff1a3aad7ed1
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-02-20 12:59:01 +0530

    core: buid the package 0.1.1 version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="limited-time-token-handler",
-    version="0.1.0",
+    version="0.1.1",
     author="shailesh",
     author_email="shaileshpandit141@gmail.com",
     description=(
